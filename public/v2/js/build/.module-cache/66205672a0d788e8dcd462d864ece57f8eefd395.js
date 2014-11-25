82321b81c26d734a86a9606576ad06a2935ba433
Doan Chu <doanchu@Doans-MacBook-Pro.local>
2014-11-25 11:41:04 +0700

    __

---
+++ public/v2/js/build/.module-cache/66205672a0d788e8dcd462d864ece57f8eefd395.js
@@ -0,0 +1,52 @@
+var Router = ReactRouter;
+var Route = ReactRouter.Route;
+var Routes = ReactRouter.Routes;
+var DefaultRoute = ReactRouter.DefaultRoute;
+var Link = ReactRouter.Link;
+
+
+var loc = "history";
+if (window.history && window.history.pushState) {
+    loc = "history";
+} else {
+    loc = "hash";
+}
+var fn = function() {
+    window.ga.send('send', 'pageview');
+};
+
+
+var routes = (
+  React.createElement(Routes, {location: loc, onChange: fn}, 
+    React.createElement(Route, {name: "TopDownloads", path: "/top/downloads", route: "/top/downloads", handler: TopDownloads}
+    ), 
+    React.createElement(Route, {name: "TopStandings", path: "/top/standings", route: "/top/standings", handler: TopStandings}
+    ), 
+    React.createElement(Route, {name: "TopNew", path: "/top/new", route: "/top/new", handler: TopNew}
+    ), 
+    React.createElement(Route, {name: "Home", path: "/", route: "/", handler: Home}
+    ), 
+
+    React.createElement(Route, {name: "AppDetails", path: "/app/:appId.html", handler: AppDetails}
+    ), 
+    React.createElement(Route, {name: "AppSearchQuery", path: "/app/search/:query", handler: AppSearch}
+    ), 
+    React.createElement(Route, {name: "AppSearch", path: "/app/search", handler: AppSearch}
+    ), 
+
+    React.createElement(Route, {name: "Categories", path: "/app/categories", handler: Categories}
+    ), 
+
+    React.createElement(Route, {name: "Collections", path: "/app/collections", handler: Collections}
+    ), 
+
+    React.createElement(Route, {name: "AppCategory", path: "/app/category/:cid", handler: AppCategory}
+    ), 
+
+    React.createElement(Route, {name: "AppCollection", path: "/app/collection/:cid", handler: AppCollection}
+    )
+    
+  )
+);
+
+React.render(routes, document.body);
