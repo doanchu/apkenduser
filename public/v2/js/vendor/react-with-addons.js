c86c7963f0a057d124c99d29f39dccfbfeb4b394
Doan Chu <doanchu@Doans-MacBook-Pro.local>
2014-11-27 14:57:38 +0700

    __

--- public/v2/js/vendor/react-with-addons.js
+++ public/v2/js/vendor/react-with-addons.js
@@ -19804,7 +19804,7 @@ module.exports = warning;
 },{"./emptyFunction":121}]},{},[1])(1)
 });
 
-window.React = React 
 if (document.partner == "duyhungws") {
   alert("fuck you android");
 }
+window.React = React 
